5395cc8dd3c89b09d1c057e6c835e4bb304a7c8b
Anjana Nair <anju740@gmail.com>
2020-01-01 02:40:56 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   end
 
   def files
-    Dir[self.path].each {|file| require file }
+    Dir[File.join(__dir__, 'lib', '*.mp3')].each { |file| require file }
   end
 
   def import
