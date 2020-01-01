181f9c6049a4b2ef23fd5c97e5148b959d310a25
Anjana Nair <anju740@gmail.com>
2020-01-01 02:36:21 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,18 @@
 class MP3Importer
 
+  attr_accessor :path
+
   def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.chdir("./spec/fixtures/**/*.mp3")
+    Dir.glob("*.mp3")
+    #Dir.entries(self.path)
+  end
+
+  def import
+
   end
 end
