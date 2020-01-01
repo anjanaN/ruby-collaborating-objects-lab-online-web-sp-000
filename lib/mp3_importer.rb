85764ed98a4a99f6d954f593c0a9d9a13e463acb
Anjana Nair <anju740@gmail.com>
2020-01-01 03:10:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,10 +7,11 @@ class MP3Importer
   end
 
   def files
-    Dir["#{self.path}/*.mp3"]
+    #Dir["#{self.path}/*.mp3"]
     #Dir.entries(self.path)
     #Dir.chdir(self.path)
     #Dir.glob("**", "*.mp3")
+    Dir.glob(self.path "/**/*.mp3")
   end
 
   def import
